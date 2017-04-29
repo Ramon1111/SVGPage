3edf0ea5d00f0c2cb07b7ad9421caf30d988cc93
Ramon1111 <jramoncito7-99@hotmail.com>
2017-04-28 23:32:11 -0500

    [FIX] Added options of show size of images

--- Resources/normalevents.js
+++ Resources/normalevents.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
   $('.parallax').parallax();
+
+  $('#boton_mostrar').click(function(){
+    $('#rectangulo_png,#rectangulo_svg').attr('class','tooltipped');
+    $('#rectangulo_png,#rectangulo_svg').attr('data-position','bottom');
+    $('#rectangulo_png,#rectangulo_svg').attr('data-delay','50');
+    $('#rectangulo_png').attr('data-tooltip',"1'161 bytes (1.13 KB)");
+    $('#rectangulo_svg').attr('data-tooltip',"267 bytes");
+    $('.tooltipped').tooltip({delay: 50});
+  });
 })
